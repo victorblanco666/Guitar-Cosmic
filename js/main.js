c0dee4a689539129d9a5e21367ba314db776653d
tito0105 <monkill2002@gmail.com>
2023-04-05 02:54:39 -0400

    good nigth 2.0xdas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-// Obtenemos el botón y el contenedor de búsqueda
+
 var btnSearch = document.getElementById("btn-search");
 var searchContainer = document.getElementById("search-container");
 
-// Agregamos un event listener para detectar el click en el botón
 btnSearch.addEventListener("click", function() {
-// Alternamos la clase "show" del contenedor de búsqueda para mostrar u ocultar la barra
+
 searchContainer.classList.toggle("show");
 });
 
