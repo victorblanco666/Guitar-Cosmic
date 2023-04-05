96059f490ef3c62350bab5bc247852afc5e503a3
tito0105 <monkill2002@gmail.com>
2023-04-05 02:51:53 -0400

    actualizacion good nigth

--- js/main.js
+++ js/main.js
@@ -8,53 +8,5 @@ btnSearch.addEventListener("click", function() {
 searchContainer.classList.toggle("show");
 });
 
-let hideText_btn = document.getElementById('hideText_btn')
 
-let hideText = document.getElementById('hideText')
 
-hideText_btn.addEventListener('click',toggleText)
-
-function toggleText() {
-    hideText.classList.toggle('show')
-
-    if(hideText.classList.contains('show')) {
-        hideText_btn.innerHTML = '- Info'
-    }
-    else {
-        hideText_btn.innerHTML = '+ Info'
-    }
-}
-
-let hideText_btn2 = document.getElementById('hideText_btn2')
-
-let hideText2 = document.getElementById('hideText2')
-
-hideText_btn2.addEventListener('click',toggleText2)
-
-function toggleText2() {
-    hideText2.classList.toggle('show')
-
-    if(hideText2.classList.contains('show')) {
-        hideText_btn2.innerHTML = '- Info'
-    }
-    else {
-        hideText_btn2.innerHTML = '+ Info'
-    }
-}
-
-let hideText_btn3 = document.getElementById('hideText_btn3')
-
-let hideText3 = document.getElementById('hideText3')
-
-hideText_btn3.addEventListener('click',toggleText3)
-
-function toggleText3() {
-    hideText3.classList.toggle('show')
-
-    if(hideText3.classList.contains('show')) {
-        hideText_btn3.innerHTML = '- Info'
-    }
-    else {
-        hideText_btn3.innerHTML = '+ Info'
-    }
-}
